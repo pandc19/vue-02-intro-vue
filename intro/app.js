d6ed6de2c2d99d918083f55e2cea0ed43fbc5816
Jonathan Pillajo <pandc19@outlook.com>
2024-07-28 21:36:05 -0500

    end section 3

---
+++ intro/app.js
@@ -0,0 +1,32 @@
+const { createApp, ref } = Vue;
+
+const app = createApp({
+  //   template: `
+  //     <h1>{{message}}</h1>
+  //     <p>- {{author}}</p>
+  //     `,
+
+  setup() {
+    const message = ref("I'm Batman");
+
+    const author = ref("Bruce Wayne");
+
+    const changeQuote = () => {
+      message.value = "Hola, soy Goku";
+      author.value = "Goku";
+    };
+
+    // setTimeout(() => {
+    //   message.value = "Hola, soy Goku";
+    //   author.value = "Goku";
+    // }, 1500);
+
+    return {
+      message,
+      author,
+      changeQuote,
+    };
+  },
+});
+
+app.mount("#myApp");
